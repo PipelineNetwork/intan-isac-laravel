9b8d44afdd79b6173cdd072cd905e808aaafc0a9
afizihusaini <afizihusaini@pipeline-network.com>
2021-09-16 21:41:06 +0800

    Add feature GetDataXMLbyIC from HRMIS to registered process

--- app/Models/Perkhidmatan.php
+++ app/Models/Perkhidmatan.php
@@ -7,13 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Perkhidmatan extends Model
 {
-    use HasFactory;
+    // use HasFactory;
 
     protected $table = 'pro_perkhidmatan';
+
+    protected $fillable = [
+        'KOD_GELARAN_JAWATAN',
+        'KOD_PERINGKAT',
+        'KOD_KLASIFIKASI_PERKHIDMATAN',
+        'KOD_GRED_JAWATAN',
+        'KOD_TARAF_PERJAWATAN',
+        'KOD_JENIS_PERKHIDMATAN',
+        'TARIKH_LANTIKAN',
+        'ID_PESERTA',
+    ];
 
     public function permohanan()
     {
         return $this->belongsTo(Permohanan::class);
-    } 
-
+    }
 }
--- app/Models/Tugas.php
+++ app/Models/Tugas.php
@@ -7,12 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tugas extends Model
 {
-    use HasFactory;
+    // use HasFactory;
 
     protected $table = 'pro_tempat_tugas';
+
+    protected $fillable = [
+        'ID_PESERTA',
+        'GELARAN_KETUA_JABATAN',
+        'KOD_KEMENTERIAN',
+        'KOD_JABATAN',
+        'BAHAGIAN',
+        'ALAMAT_1',
+        'ALAMAT_2',
+        'ALAMAT_3',
+        'POSKOD',
+        'BANDAR',
+        'KOD_NEGERI',
+        'KOD_NEGARA',
+        'NAMA_PENYELIA',
+        'EMEL_PENYELIA',
+        'NO_TELEFON_PENYELIA',
+        'NO_FAX_PENYELIA',
+        'STATUS_PUSH_PULL',
+    ];
 
     public function permohanan()
     {
         return $this->belongsTo(Permohanan::class);
-    } 
+    }
 }
