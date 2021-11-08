95acaeff8a09d0a6d53eba8e664d6273dfe8b460
njhn-pnsb <najhan.najib@pipeline-network.com>
2021-11-08 18:04:34 +0800

    CL - add seeder for notifikasi email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('notifikasi_emails')->insert([
+            
             'id' => 1,
+            'tawaran_penilaian_individu' => 7,
+            'tawaran_penilaian_kumpulan' => 7,
+            'peringatan_penilaian' => 3,
+            'peringatan_tidak_hadir' => 1,
+            'jadual_penilaian' => 0,
         ]);
     }
 }
