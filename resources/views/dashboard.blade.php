a5a1d93a86a449279c5a2fd732b82786397cd78c
WanHuzairipipe <wan.huzairi@pipeline.com.my>
2022-01-19 15:36:38 +0800

    show jumlah markah lulus dan keseluruhan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -298,88 +298,9 @@
                         </div>
                     </div>
                 </div>
+                <iframe id="customer-chatbox"></iframe>
             </div>
         @endunlessrole
-        {{-- <div class="row">
-            <div class="col">
-                <div class="card mt-3">
-                    <div class="card-header" style="background-color:#FFA500;">
-                        <b class="text-white">Video</b>
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-group">
-                            @foreach ($videodannotas as $video)
-                                @if ($video->jenis == 'Video')
-                                    <li class="list-group-item">
-                                        <div class="row">
-                                            <div class="col text-center">
-                                                <video width="500" controls>
-                                                    <source src="/storage/{{ $video->video }}" type="video/mp4">
-                                                    Penyemak imbas anda tidak menyokong video HTML.
-                                                </video>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col">
-                                                <h3>{{ $video->tajuk }}</h3>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col">
-                                                <p>{{ $video->nota }}</p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                @endif
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-
-        {{-- <div class="row">
-            <div class="col">
-                <div class="card m-3">
-                    <div class="card-header" style="background-color:#FFA500;">
-                        <b class="text-white">Nota</b>
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-group">
-                            @foreach ($videodannotas as $nota)
-                                @if ($nota->jenis == 'Nota')
-                                    <li class="list-group-item"> --}}
-
-
-
-
-        {{-- jgn delete --}}
-        {{-- <div class="row">
-                                <div class="col text-center">
-                                    <embed src="/storage/{{$nota->video}}" width="500px" height="500px" />
-                                </div>
-                            </div> --}}
-        {{-- <div class="row">
-                                            <div class="col">
-                                                <h3>{{ $nota->tajuk }}</h3>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col">
-                                                <p>{{ $nota->nota }}</p>
-                                            </div>
-                                        </div>
-                                        <a href="/storage/{{ $nota->video }}" class="btn bg-gradient-info"><i
-                                                class="fas fa-file"></i> Klik disini untuk nota</a>
-                                       
-                                    </li>
-                                @endif
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
     </div>
 
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
@@ -529,4 +450,6 @@
 
         }); // end am4core.ready()
     </script>
+
+    <script src="https://isacsupport.intan.my/chat_widget.js"></script>
 @stop
