857386f23511c07d21417547d38c9fdb5f1afc5d
WanHuzairipipe <wan.huzairi@pipeline.com.my>
2022-03-11 17:51:11 +0800

    fix lot of coding 5

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,18 +1,67 @@
 <!doctype html>
-<title>Intan - Site Maintenance</title>
-<style>
-  body { text-align: center; padding: 150px; }
-  h1 { font-size: 50px; }
-  body { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-  a { color: #dc8100; text-decoration: none; }
-  a:hover { color: #333; text-decoration: none; }
-</style>
 
-<article>
-    <h1>Aplikasi web ISAC sedang diselenggara!</h1>
-    <div>
-        <p>Maaf atas kesulitan ini. Kami sedang melakukan beberapa penyelenggaraan buat masa ini. Jika ada sebarang pertanyaan, anda boleh e-melkan kepada kami melalui <u>isachelp@intanbk.intan.my</u> dan disalin (cc) <u>dlisachelp@intanbk.intan.my</u>, kami akan kembali dalam talian sebentar lagi!</p>
-        <p>&mdash; Urusetia ISAC</p>
-    </div>
-</article>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
+    {{-- <link rel="icon" type="image/png" href="../../img/intan.png"> --}}
+    <link rel="icon" type="image/png" href="../../img/intan.png">
+    <title>Intan - Site Maintenance</title>
+
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="../../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="../../assets/css/nucleo-svg.css" rel="stylesheet" />
+
+    <style>
+        body {
+            text-align: center;
+            padding: 150px;
+        }
+
+        h1 {
+            font-size: 50px;
+        }
+
+        body {
+            font: 20px Helvetica, sans-serif;
+            color: #333;
+        }
+
+        article {
+            display: block;
+            text-align: left;
+            width: 650px;
+            margin: 0 auto;
+        }
+
+        a {
+            color: #dc8100;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #333;
+            text-decoration: none;
+        }
+
+    </style>
+</head>
+
+<body>
+    <article>
+        <i class="fa-solid fa-triangle-exclamation"></i>
+        <h1>Sistem ISAC sedang diselenggara!</h1>
+        <div>
+            <p>Maaf atas kesulitan ini. Kami sedang melakukan beberapa penyelenggaraan buat masa ini. Jika ada sebarang
+                pertanyaan, anda boleh e-melkan kepada kami melalui <u>isachelp@intanbk.intan.my</u> dan disalin (cc)
+                <u>dlisachelp@intanbk.intan.my</u>, kami akan kembali dalam talian sebentar lagi!
+            </p>
+            <p>&mdash; Urusetia ISAC</p>
+        </div>
+    </article>
+</body>
